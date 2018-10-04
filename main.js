a3e711b617bc607e408598fd3ded756691e53925
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2018-10-03 19:55:25 -0500

    add R.md, main.js

---
+++ main.js
@@ -0,0 +1,56 @@
+const greaterNum = (x, y) => {
+  if (x > y) {
+    return (`The greater number of ${x} and ${y} is ${x}`)
+  } else {
+    return (`The greater number of ${y} and ${x} is ${y}`)
+  }
+}
+
+console.log(greaterNum(9,8))
+console.log(greaterNum(3,5))
+
+
+/* expected 
+output The greater number of 5 and 10 is 10.
+*/
+
+
+const helloWorld = (code) =>{
+  if (code === 'es') {
+    return ('Hola a todos!')
+  } else if (code === 'it') {
+    return ('Ciao tutti!')
+  } else {
+    return ('Hello everyone!')
+  }
+}
+
+console.log(helloWorld('en'))
+console.log(helloWorld('it'))
+console.log(helloWorld('es'))
+
+/* expected output 
+Hola a todos, --> es
+Ciao, tutti, ---> it
+Hello everyone ---> en
+*/
+
+const pluralizer = (noun, number) => {
+   if (number > 1) {
+     return (`${number} ${noun + 's'}`)
+   } else if (number < 1) {
+    return (noun)
+   } else {
+     return(`${number} ${noun}`)
+   }
+}
+
+console.log(pluralizer('casa', 9))
+console.log(pluralizer('casa', 2))
+console.log(pluralizer('casa', 1))
+console.log(pluralizer('casa', 0))
+
+
+/* expected output
+"5 cats" or "1 dog".
+*/
