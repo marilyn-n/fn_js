b376ff3fb0af92051bbdd76ca30654acb91e5e4b
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2018-10-15 22:40:35 -0500

    create README.md

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-// JS Fucntions
+# JS Functions
 
+Experiments and exercises with function expression and FARTs.
+
+```js
+
+const argsLength = function (p1, p2, p3) {
+  console.log(p1, p2, p3)
+  console.log(arguments);
+  console.log(arguments.length);
+  return arguments.length
+}
+
+argsLength()
+```
+
+```js
+const uniqueNumbers = (arr) => {
+  const mySet = new Set(arr)
+  return mySet
+}
+
+uniqueNumbers([1, 1, 3, 4, 5, 5, 5, 5, 8, 9, 0, 0, 0])
+
+```
--- main.js
+++ main.js
@@ -6,8 +6,8 @@ const greaterNum = (x, y) => {
   }
 }
 
-console.log(greaterNum(9,8))
-console.log(greaterNum(3,5))
+console.log(greaterNum(9, 8))
+console.log(greaterNum(3, 5))
 
 
 /* expected 
@@ -15,7 +15,7 @@ output The greater number of 5 and 10 is 10.
 */
 
 
-const helloWorld = (code) =>{
+const helloWorld = (code) => {
   if (code === 'es') {
     return ('Hola a todos!')
   } else if (code === 'it') {
@@ -36,13 +36,13 @@ Hello everyone ---> en
 */
 
 const pluralizer = (noun, number) => {
-   if (number > 1) {
-     return (`${number} ${noun + 's'}`)
-   } else if (number < 1) {
+  if (number > 1) {
+    return (`${number} ${noun + 's'}`)
+  } else if (number < 1) {
     return (noun)
-   } else {
-     return(`${number} ${noun}`)
-   }
+  } else {
+    return (`${number} ${noun}`)
+  }
 }
 
 console.log(pluralizer('casa', 9))
@@ -55,16 +55,56 @@ console.log(pluralizer('casa', 0))
 "5 cats" or "1 dog".
 */
 
+const counter = (str) => {
+  for (let i = 0; i < str.length; i++) {
+
+    const elem = str[i]
+    const count = str[i].match(elem).length
+
+    const res = count + ' : ' + elem
+    console.log(res)
+  }
+}
+
+
+console.log(counter('success'))
+
+var temp = "This is a string.";
+for (let i = 0; i < temp.length; i++) {
+
+}
+
+var count = (temp.match(/i/g) || []).length;
+console.log(count);
+
 // expected output 'success'
 // expected output s: 3, u: 1, e:1 c: 2
 
-const counter = (str) => {
+const uniqueLetters = (str) => {
   const myArray = str.split('')
   const mySet = new Set(myArray)
   return mySet
 }
 
-console.log(counter('success'))
+console.log(uniqueLetters('success'))
 
 // expected output 'success'
 // s u c e
+
+const uniqueNumbers = (arr) => {
+  const mySet = new Set(arr)
+
+  return mySet
+}
+
+console.log(uniqueNumbers([1, 1, 3, 4, 5, 5, 5, 5, 8, 9, 0, 0, 0]))
+
+
+const argsLength = function (p1, p2, p3) {
+  console.log(p1, p2, p3)
+  console.log(arguments);
+  console.log(arguments.length);
+  return arguments.length
+}
+
+argsLength()
